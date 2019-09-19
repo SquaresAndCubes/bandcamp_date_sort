d0fcd8ac8d7abecbb0e708964a22a058f75119e3
Brent Vaalburg <btvaalburg@gmail.com>
2019-09-18 22:06:24 -0600

    regex finds dates

--- content.js
+++ content.js
@@ -3,8 +3,8 @@
 //function which sorts parent noded by child nodes nested text///////////////////
 function sortUsingNestedText(parent, childSelector, keySelector, sort = "desc") {
   var items = parent.children(childSelector).sort(function(a, b) {
-    var vA = $(keySelector, a).text();
-    var vB = $(keySelector, b).text();
+    var vA = $(keySelector, a).text().match(/([12]\d{3}.(0[1-9]|1[0-2]).(0[1-9]|[12]\d|3[01]))/);
+    var vB = $(keySelector, b).text().match(/([12]\d{3}.(0[1-9]|1[0-2]).(0[1-9]|[12]\d|3[01]))/);
     if (sort == "desc") {
       return (vA > vB) ? -1 : (vA < vB) ? 1 : 0;
     } else if (sort == "asc") {
