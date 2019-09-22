88d40d8b74a5fc326987a310f7cd6802aa3613d0
Brent Vaalburg <btvaalburg@gmail.com>
2019-09-22 14:09:13 -0600

    +date regex

--- content.js
+++ content.js
@@ -1,21 +1,32 @@
 //BandCamp Date Sort Script//
 
 //regular expression for finding dates in the titles of shows
-var dateRegex = /([12]\d{3}.(0[1-9]|1[0-2]).(0[1-9]|[12]\d|3[01]))/
+var dateRegex = /([12]\d{3}.(0[1-9]|1[0-2]).(0[1-9]|[12]\d|3[01]))/;
 
 //function which sorts parent noded by child nodes nested text///////////////////
-function sortUsingNestedText(parent, childSelector, keySelector, sort = "desc") {
+function sortUsingNestedText(parent, childSelector, keySelector) {
   var items = parent.children(childSelector).sort(function(a, b) {
     var vA = $(keySelector, a).text();
     var vB = $(keySelector, b).text();
-    if (sort == "desc") {
-      return (vA > vB) ? -1 : (vA < vB) ? 1 : 0;
-    } else if (sort == "asc") {
-      return (vA < vB) ? -1 : (vA > vB) ? 1 : 0;
+    var vC = '';
+    var vD = '';
+    if (vA.match(dateRegex)) {
+      var vC = vA.match(dateRegex)[0];
+    } else {
+      vC = vA;
     }
+    if (vB.match(dateRegex)) {
+      var vD = vB.match(dateRegex)[0];
+    } else {
+      vD = vB;
+    }
+
+    console.log(vC + '\n' + vD);
+    return (vC > vD) ? -1 : (vC < vD) ? 1 : 0;
   });
   parent.append(items);
 }
+
 
 
 //function for scrolling the page all the way down////////
