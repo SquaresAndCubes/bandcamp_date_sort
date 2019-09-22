949b946aa1a5d409c49e798e8541ce0a1265109a
Brent Vaalburg <btvaalburg@gmail.com>
2019-09-22 11:44:57 -0600

    -remove regex

--- content.js
+++ content.js
@@ -6,8 +6,8 @@ var dateRegex = /([12]\d{3}.(0[1-9]|1[0-2]).(0[1-9]|[12]\d|3[01]))/
 //function which sorts parent noded by child nodes nested text///////////////////
 function sortUsingNestedText(parent, childSelector, keySelector, sort = "desc") {
   var items = parent.children(childSelector).sort(function(a, b) {
-    var vA = $(keySelector, a).text().match(dateRegex);
-    var vB = $(keySelector, b).text().match(dateRegex);
+    var vA = $(keySelector, a).text();
+    var vB = $(keySelector, b).text();
     if (sort == "desc") {
       return (vA > vB) ? -1 : (vA < vB) ? 1 : 0;
     } else if (sort == "asc") {
@@ -16,7 +16,6 @@ function sortUsingNestedText(parent, childSelector, keySelector, sort = "desc") 
   });
   parent.append(items);
 }
-/////////////////////////////////////////////////////////////////////////////////
 
 
 //function for scrolling the page all the way down////////
